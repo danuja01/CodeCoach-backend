66734a7a042a2f53b1c37e5c66b0267a8821ed31
Sachintha Dasanayake <it21050794@my.sliit.lk>
2023-09-07 22:39:46 +0530

    Feat: Add new feature

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,6 +4,7 @@ import { protect } from '@/middleware/auth';
 import authRouter from './auth.routes';
 import submissionRouter from './submission.routes';
 import challengeRouter from './challenge.routes';
+import labRouter from './lab.routes';
 import userRouter from './user.routes';
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.use('/auth', authRouter);
 router.use('/users', protect, userRouter);
 router.use('/submissions', protect, submissionRouter);
 router.use('/challenges', protect, challengeRouter);
+router.use('/labs', protect, labRouter);
 
 export default router;
---
+++ src/services/lab.js
@@ -0,0 +1,49 @@
+import createError from 'http-errors';
+import { createLab, deleteLab, getAllLabs, getLabById, getLabsByUser, updateLab } from '@/repository/lab';
+
+export const createLabService = async (labDetails) => {
+  const lab = await createLab(labDetails);
+  return lab;
+};
+
+export const getLabsByUserService = async (user) => {
+  if (!user) throw new createError(404, 'User not found');
+
+  let labs = [];
+
+  if (user.role === 'LECTURER') {
+    // If the user is a lecturer, fetch labs created by the lecturer
+    labs = await getLabsByUser(user);
+  } else if (user.role === 'STUDENT') {
+    // If the user is a student, fetch labs assigned to the student
+    labs = await getLabsByUser({ students: user._id });
+  }
+
+  return labs;
+};
+
+export const getAllLabsService = async () => {
+  try {
+    const labs = await getAllLabs({});
+    return labs;
+  } catch (error) {
+    throw new createError(500, 'Error while fetching labs');
+  }
+};
+
+export const getLabByIdService = async (labId) => {
+  const lab = await getLabById(labId);
+  if (!lab) throw new createError(404, 'Lab not found');
+  return lab;
+};
+
+export const updateLabService = async (labId, labDetails) => {
+  const updatedLab = await updateLab(labId, labDetails);
+  if (!updatedLab) throw new createError(422, 'Invalid lab ID');
+  return updatedLab;
+};
+
+export const deleteLabService = async (labId) => {
+  const result = await deleteLab(labId);
+  if (!result) throw new createError(422, 'Invalid lab ID');
+};

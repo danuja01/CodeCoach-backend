4d0a70f8bce8ac0c87dce15c6876fe968ae96108
Supun Kariyawasam <bhasura2105@gmail.com>
2023-09-09 10:17:38 +0530

    Patch: patched the getTestCases endpoint

--- src/routes/challenge.routes.js
+++ src/routes/challenge.routes.js
@@ -9,7 +9,7 @@ import {
   removeChallenge,
   updateChallenge
 } from '@/controllers/challenge';
-// import { authorizer } from '@/middleware';
+import { authorizer } from '@/middleware';
 import { addChallengeSchema, challengeIdSchema, updateChallengeSchema } from '@/validations/challenge';
 
 const challengeRouter = express.Router();
@@ -17,40 +17,37 @@ const challengeRouter = express.Router();
 challengeRouter.post(
   '/',
   celebrate({ [Segments.BODY]: addChallengeSchema }),
-  // authorizer(['ADMIN', 'LECTURER']),
+  authorizer(['ADMIN', 'LECTURER']),
   tracedAsyncHandler(createChallenge)
 );
 
 challengeRouter.get(
   '/testCases/:id',
   celebrate({ [Segments.PARAMS]: challengeIdSchema }),
+  authorizer(['ADMIN', 'LECTURER', 'STUDENT']),
   tracedAsyncHandler(getTestCases)
 );
 
 challengeRouter.get(
   '/:id',
   celebrate({ [Segments.PARAMS]: challengeIdSchema }),
-  // authorizer(['ADMIN', 'LECTURER', 'STUDENT']),
+  authorizer(['ADMIN', 'LECTURER', 'STUDENT']),
   tracedAsyncHandler(getById)
 );
 
-challengeRouter.get(
-  '/',
-  // authorizer(['ADMIN', 'LECTURER', 'STUDENT']),
-  tracedAsyncHandler(getAll)
-);
+challengeRouter.get('/', authorizer(['ADMIN', 'LECTURER', 'STUDENT']), tracedAsyncHandler(getAll));
 
 challengeRouter.put(
   '/:id',
   celebrate({ [Segments.PARAMS]: challengeIdSchema, [Segments.BODY]: updateChallengeSchema }),
-  // authorizer(['ADMIN', 'LECTURER']),
+  authorizer(['ADMIN', 'LECTURER']),
   tracedAsyncHandler(updateChallenge)
 );
 
 challengeRouter.delete(
   '/:id',
   celebrate({ [Segments.PARAMS]: challengeIdSchema }),
-  // authorizer(['ADMIN', 'LECTURER']),
+  authorizer(['ADMIN', 'LECTURER']),
   tracedAsyncHandler(removeChallenge)
 );
 
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -11,6 +11,6 @@ const router = express.Router();
 router.use('/auth', authRouter);
 router.use('/users', protect, userRouter);
 router.use('/submissions', protect, submissionRouter);
-router.use('/challenges', challengeRouter);
+router.use('/challenges', protect, challengeRouter);
 
 export default router;
